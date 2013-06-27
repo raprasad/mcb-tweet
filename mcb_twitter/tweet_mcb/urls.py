18ae47ccdb366935d1a6ea31b450a1b9e635b636
Raman Prasad <raman_prasad@harvard.edu>
2013-06-27 14:43:19 -0400

    more path fixing

--- mcb_twitter/tweet_mcb/urls.py
+++ mcb_twitter/tweet_mcb/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns(
-    'tweet_mcb.views'
+    'mcb_twitter.tweet_mcb.views'
     , url(r'^console/$', 'view_tweet_console', name='view_tweet_console')
 
     , url(r'^console-tweeted/$', 'view_tweet_success', name='view_tweet_success')
